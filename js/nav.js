9c78aa023df851ea614980556744e0a12ba62a2c
Kolby Kruger <kolbykruger@gmail.com>
2022-03-21 13:56:16 -0400

    init design #1

--- js/nav.js
+++ js/nav.js
@@ -1,31 +1,113 @@
 //Nav
 //$( "ul.top .trigger" ).each(function(){$( this ).children('a:first').attr( "onclick", "return false" );});
 
-$('.nav .drop_menu').each(function() {
-   if ($(this).find(".item").length >= 8) {
-       $(this).addClass('column_nav');
-   }
-});
+// $('.nav .drop').each(function () {
+//     if ($(this).find('li').length >= 8) {
+//         $(this).addClass('column_nav')
+//     }
+// })
 
-var hoverTimeout;
-$('.nav .trigger').hover(function() {
-    clearTimeout(hoverTimeout);
-    $(this).addClass('open').find('.drop').attr('aria-hidden', 'true').attr('aria-expanded', 'false');
-    $('.trigger').not(this).removeClass("open");
-}, function() {
-    var $self=$(this);
-    hoverTimeout = setTimeout(function() {
-        $self.removeClass('open').find('.drop').attr('aria-hidden', 'true').attr('aria-expanded', 'false');
-    }, 250);
-});
+// var hoverTimeout
+// $('.nav .trigger').hover(
+//     function () {
+//         clearTimeout(hoverTimeout)
+//         $(this).addClass('open').find('.drop').attr('aria-hidden', 'true').attr('aria-expanded', 'false')
+//         $('.trigger').not(this).removeClass('open')
+//     },
+//     function () {
+//         var $self = $(this)
+//         hoverTimeout = setTimeout(function () {
+//             $self.removeClass('open').find('.drop').attr('aria-hidden', 'true').attr('aria-expanded', 'false')
+//         }, 250)
+//     }
+// )
 
-$('.nav .trigger > a').focusin(function() {
-    var parent = $(this).parent();
-    parent.addClass('open').find('.drop').attr('aria-hidden','false').attr('aria-expanded', 'true');
-    $('.trigger').not(parent).removeClass("open");
-});
+// $('.nav .trigger > a').focusin(function () {
+//     var parent = $(this).parent()
+//     parent.addClass('open').find('.drop').attr('aria-hidden', 'false').attr('aria-expanded', 'true')
+//     $('.trigger').not(parent).removeClass('open')
+// })
 
-$('.nav .drop a:last,.nav .drop input').focusout(function() {
-    var parent = $(this).parents().find('.trigger');
-    parent.removeClass('open').find('.drop').attr('aria-hidden','true').attr('aria-expanded', 'false');
-});
+// $('.nav .drop a:last,.nav .drop input').focusout(function () {
+//     var parent = $(this).parents().find('.trigger')
+//     parent.removeClass('open').find('.drop').attr('aria-hidden', 'true').attr('aria-expanded', 'false')
+// })
+
+const menu = document.querySelector('.menu')
+const menuPanelsNodeList = document.querySelectorAll('.menu .menu-panel')
+const menuPanels = Array.from(menuPanelsNodeList)
+const navTriggersNodeList = document.querySelectorAll('.nav .trigger a')
+const navTriggers = Array.from(navTriggersNodeList)
+
+// Menu panels
+const menuPanelLinksNodeList = document.querySelectorAll('.menu .menu-panel a')
+const menuPanelLinks = Array.from(menuPanelLinksNodeList)
+
+// Menu exit
+const menuExitButton = document.querySelector('.menu .menu-exit')
+
+navTriggers.forEach(trigger => {
+    trigger.addEventListener('click', () => {
+        const id = trigger.getAttribute('data-id')
+
+        // check if it's already open
+        // if it is, close it
+        if (trigger.classList.contains('open')) {
+            closeMenuPanels()
+        } else {
+            openMenuPanels({ trigger, id })
+        }
+    })
+})
+
+function closeMenuPanels() {
+    navTriggers.forEach(trigger => {
+        trigger.classList.remove('open')
+    })
+    menuPanels.forEach(panel => {
+        panel.classList.remove('open')
+    })
+    menu.classList.remove('open')
+    document.body.classList.remove('menu-open')
+}
+function openMenuPanels(props) {
+    // remove open from all triggers
+    navTriggers.forEach(trigger => {
+        trigger.classList.remove('open')
+    })
+    // remove open from all menu panels
+    menuPanels.forEach(panel => {
+        panel.classList.remove('open')
+    })
+
+    // add props to appropriate elements
+    props.trigger.classList.add('open')
+    menuPanels[props.id].classList.add('open')
+    menu.classList.add('open')
+    document.body.classList.add('menu-open')
+}
+
+// Menu panel
+menuPanelLinks.forEach(link => {
+    const id = link.getAttribute('data-id')
+    if (id) {
+        link.addEventListener('mouseenter', () => {
+            const menuPanelImagesNodeList = link.closest('.links').nextElementSibling.querySelectorAll('li')
+            const menuPanelImages = Array.from(menuPanelImagesNodeList)
+
+            if (menuPanelImages) {
+                menuPanelImages.forEach(image => {
+                    image.classList.remove('active')
+                })
+
+                const relatedImage = menuPanelImages[id]
+                relatedImage.classList.add('active')
+            }
+        })
+    }
+})
+
+// Menu exit
+menuExitButton.addEventListener('click', () => {
+    closeMenuPanels()
+})
--- js/scripts.js
+++ js/scripts.js
@@ -1,78 +1,115 @@
-//LINK KILLER -- Remove after Homepage Design Approval
-$(document).ready(function() {$('a').click(function(e) {e.preventDefault();});})
+//Section Scroll Animations (requires scrollmagic)
+document.addEventListener('DOMContentLoaded', function () {
+    var controller = new ScrollMagic.Controller()
+    var elems = document.querySelectorAll('section')
+
+    for (var i = 0; i < elems.length; i++) {
+        new ScrollMagic.Scene({
+            triggerElement: elems[i],
+            triggerHook: 0.85,
+            reverse: true,
+        })
+            .setClassToggle(elems[i], 'section-active')
+            .addTo(controller)
+    }
+})
 
 //Blog Categories (if a selectbox)
 let blogCategories = {
     elem: document.querySelector('select#blog_categories'),
-    init: function() {
+    init: function () {
         if (this.elem) {
-            this.elem.addEventListener('change', function(e) {
+            this.elem.addEventListener('change', function (e) {
                 window.location.href = window.location.href.split('?')[0] + '?category=' + e.target.value
-            });
+            })
         }
-    }
-}.init();
+    },
+}.init()
 
-//Populate Blog Category select 
+//Populate Blog Category select
 var getUrlParameter = function getUrlParameter(sParam) {
     var sPageURL = window.location.search.substring(1),
         sURLVariables = sPageURL.split('&'),
         sParameterName,
-        i;
+        i
 
     for (i = 0; i < sURLVariables.length; i++) {
-        sParameterName = sURLVariables[i].split('=');
+        sParameterName = sURLVariables[i].split('=')
 
         if (sParameterName[0] === sParam) {
-            return typeof sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1]);
+            return typeof sParameterName[1] === undefined ? true : decodeURIComponent(sParameterName[1])
         }
     }
-    return false;
-};
-var blogCat = getUrlParameter('category');
-if(blogCat){
-$('#blog_categories').val(blogCat);
-}else{
-$('#blog_categories').val($("#blog_categories option:first").val());    
+    return false
+}
+var blogCat = getUrlParameter('category')
+if (blogCat) {
+    $('#blog_categories').val(blogCat)
+} else {
+    $('#blog_categories').val($('#blog_categories option:first').val())
 }
 
 //Responsive iFrame
-$('iframe[src*="youtube"],iframe[src*="vimeo"]').wrap('<div class="responsive-iframe"/>');
+$('iframe[src*="youtube"],iframe[src*="vimeo"]').wrap('<div class="responsive-iframe"/>')
 
 //Accordion
-document.addEventListener('DOMContentLoaded', function() {
-    let accordion = document.querySelectorAll('.accordion-title');
+document.addEventListener('DOMContentLoaded', function () {
+    let accordion = document.querySelectorAll('.accordion-title')
 
     for (let i = 0; i < accordion.length; i++) {
-        accordion[i].addEventListener('click', function() {
-            let panel = this.nextElementSibling;
+        accordion[i].addEventListener('click', function () {
+            let panel = this.nextElementSibling
 
-            if (panel.style.maxHeight){
-                this.classList.remove('open');
-                panel.style.maxHeight = null;
-                panel.setAttribute('aria-hidden', true);
-                panel.setAttribute('aria-expanded', false);
+            if (panel.style.maxHeight) {
+                this.classList.remove('open')
+                panel.style.maxHeight = null
+                panel.setAttribute('aria-hidden', true)
+                panel.setAttribute('aria-expanded', false)
             } else {
-                this.classList.add('open');
-                panel.style.maxHeight = panel.scrollHeight + 'px';
-                panel.setAttribute('aria-hidden', false);
-                panel.setAttribute('aria-expanded', true);
+                this.classList.add('open')
+                panel.style.maxHeight = panel.scrollHeight + 'px'
+                panel.setAttribute('aria-hidden', false)
+                panel.setAttribute('aria-expanded', true)
             }
-        });
+        })
     }
-});
+})
 
 //Flickity Carousel
-$('.carousel .group').flickity({
+$('.carousel .group .images').flickity({
     cellSelector: '.slide',
     wrapAround: true,
     adaptiveHeight: false,
     cellAlign: 'center',
     prevNextButtons: false,
+    pageDots: false,
+    imagesLoaded: true,
+    autoPlay: false,
+    selectedAttraction: 0.01,
+    friction: 0.2,
+    asNavFor: '.carousel .group .details',
+    contain: true,
+    draggable: false,
+})
+
+let isDraggable = window.matchMedia('(max-width: 968px)').matches
+window.addEventListener('resize', function () {
+    isDraggable = window.matchMedia('(max-width: 968px)').matches
+})
+
+$('.carousel .group .details').flickity({
+    cellSelector: '.slide',
+    wrapAround: true,
+    adaptiveHeight: false,
+    cellAlign: 'center',
+    prevNextButtons: true,
     pageDots: true,
     imagesLoaded: true,
-    autoPlay: 8000,
-});
+    autoPlay: false,
+    selectedAttraction: 0.01,
+    friction: 0.2,
+    draggable: isDraggable,
+})
 
 $('.slideshow .container').flickity({
     cellSelector: '.slide',
@@ -83,29 +120,42 @@ $('.slideshow .container').flickity({
     pageDots: true,
     imagesLoaded: true,
     autoPlay: 5000,
-});
+})
+
+$('.stories .container').flickity({
+    cellSelector: '.group',
+    wrapAround: true,
+    adaptiveHeight: false,
+    cellAlign: 'center',
+    prevNextButtons: true,
+    pageDots: true,
+    imagesLoaded: true,
+    autoPlay: 5000,
+    selectedAttraction: 0.01,
+    friction: 0.2,
+})
 
 //Universal Tables
-$('table').wrap("<div class='universal-table'></div>");
+$('table').wrap("<div class='universal-table'></div>")
 
 //PDO Page loader
-document.addEventListener('DOMContentLoaded', function() {
-    let pdoElement = document.getElementById('pdopage');
+document.addEventListener('DOMContentLoaded', function () {
+    let pdoElement = document.getElementById('pdopage')
     if (pdoElement) {
-        let loadState = document.createElement('div');
-            loadState.classList.add('pdo-loader');
-            loadState.setAttribute('aria-hidden', true);
-            //loadState.textContent = 'Loading';
+        let loadState = document.createElement('div')
+        loadState.classList.add('pdo-loader')
+        loadState.setAttribute('aria-hidden', true)
+        //loadState.textContent = 'Loading';
 
-        pdoElement.appendChild(loadState);
+        pdoElement.appendChild(loadState)
 
         if (pdoPage) {
-            pdoPage.callbacks['before'] = function(config) {
-                document.querySelector('.pdo-loader').classList.add('pdo-loading');
-            };
-            pdoPage.callbacks['after'] = function(config) {
-                document.querySelector('.pdo-loader').classList.remove('pdo-loading');
-            };
+            pdoPage.callbacks['before'] = function (config) {
+                document.querySelector('.pdo-loader').classList.add('pdo-loading')
+            }
+            pdoPage.callbacks['after'] = function (config) {
+                document.querySelector('.pdo-loader').classList.remove('pdo-loading')
+            }
         }
     }
-});
+})
